23c5074cd1af624182995dfe360bb9e77aacea40
nguyendminh0101@gmail.com <nguyendminh0101@gmail.com>
2021-07-11 23:11:57 +0700

    xyz

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -18,27 +18,85 @@ namespace NguyenDinhMinh_Bigschool.Controllers
             _dbContext = new ApplicationDbContext();
         }
         // GET: Courses
+        
+        public ActionResult Attending()
+        {
+            var userId = User.Identity.GetUserId();
+            var course = _dbContext.Attendances
+                .Where(a => a.AttendeeId == userId)
+                .Select(a => a.Course)
+                .Include(l => l.Lecturer)
+                .Include(l => l.Category)
+                .ToList();
+            var viewModel = new CoursesViewModel
+            {
+                UpcomingCourse = course,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+            return View(viewModel);
+        }
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var course = _dbContext.Courses
+                .Where(c => c.LecturerId == userId && c.DateTime > DateTime.Now)
+                .Include(l => l.Lecturer)
+                .Include(l => l.Category)
+                .ToList();
+
+            return View(course);
+        }
+        /*[Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]*/
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList(),
+                Heading = "Add Course"
+            };
+            return View(viewModel);
+        }
+        public ActionResult Edit(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var course = _dbContext.Courses.Single(c => c.Id == id && c.LecturerId == userId);
+
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList(),
+                Date = course.DateTime.ToString("dd/M/yy"),
+                Time = course.DateTime.ToString("HH:mm"),
+                Category = course.CategoryId,
+                Place = course.Place,
+                Heading = "Edit Course",
+                Id = course.Id
+            };
+            return View("Create", viewModel);
+        }
+
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(CourseViewModel viewModel)
+        public ActionResult Update(CourseViewModel viewModel)
         {
             if (!ModelState.IsValid)
             {
                 viewModel.Categories = _dbContext.Categories.ToList();
                 return View("Create", viewModel);
             }
-            var course = new Course
-            {
-                LecturerId = User.Identity.GetUserId(),
-                DateTime = viewModel.GetDateTime(),
-                CategoryId = viewModel.Category,
-                Place = viewModel.Place
-            };
-            _dbContext.Courses.Add(course);
+            var userId = User.Identity.GetUserId();
+            var course = _dbContext.Courses.Single(c => c.Id == viewModel.Id && c.LecturerId == userId);
+            course.Place = viewModel.Place;
+            course.DateTime = viewModel.GetDateTime();
+            course.CategoryId = viewModel.Category;
             _dbContext.SaveChanges();
-            return RedirectToAction("Index","Home");
+            return RedirectToAction("Index", "Home");
+
+
         }
+
         // GET: Courses
         public ActionResult Index()
         {
--- Models/Course.cs
+++ Models/Course.cs
@@ -9,6 +9,7 @@ namespace NguyenDinhMinh_Bigschool.Models
     public class Course
     {
         public int Id { get; set; }
+        public bool IsCanceled { get; set; }
         public ApplicationUser Lecturer { get; set; }
         public string LecturerId { get; set; }
         [Required]
